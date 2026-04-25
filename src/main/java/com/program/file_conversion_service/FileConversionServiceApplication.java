958c2621db1c94bda3af38b5dff62131d722dd7d
tevsvet <naumkina.alina601@gmail.com>
2026-04-25 16:40:21 +0300

    Add ConvertRequest validation and Eureka client setup

--- src/main/java/com/program/file_conversion_service/FileConversionServiceApplication.java
+++ src/main/java/com/program/file_conversion_service/FileConversionServiceApplication.java
@@ -2,12 +2,10 @@ package com.program.file_conversion_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableEurekaClient
 @ConfigurationPropertiesScan
 @EnableScheduling
 public class FileConversionServiceApplication {
